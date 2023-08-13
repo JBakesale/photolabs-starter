bd93c2c8de59e4a451c0f6cd0fd31b527560a264
Joshua Baker <joshuadfbaker@gmail.com>
2023-08-13 11:58:24 -0700

    nav bar added, favbadge tinkering, photoList debugging needed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,29 +1,16 @@
 import React from "react";
 
-import PhotoListItem from "./components/PhotoListItem";
+
 import "./App.scss";
 import PhotoList from "components/PhotoList";
+import TopNavigation from "components/TopNavigationBar";
 
-// const sampleDataForPhotoListItem = {
-//   id: "1",
-//   location: {
-//     city: "Montreal",
-//     country: "Canada",
-//   },
-//   imageSource: `${process.env.PUBLIC_URL}/Image-1-Regular.jpeg`,
-//   username: "Joe Example",
-//   profile: `${process.env.PUBLIC_URL}/profile-1.jpg`,
-// };
 
-// const photos = [
-//   sampleDataForPhotoListItem,
-//   sampleDataForPhotoListItem,
-//   sampleDataForPhotoListItem,
-// ];
 
 const App = () => {
   return (
     <div className="App">
+      <TopNavigation />
       <PhotoList />
     </div>
   );
--- frontend/src/components/TopNavigationBar.jsx
+++ frontend/src/components/TopNavigationBar.jsx
@@ -1,13 +1,28 @@
 import React from 'react';
 
+import TopicList from "./TopicList";
+import FavBadge from "./FavBadge";
 import '../styles/TopNavigationBar.scss'
 
-const TopNavigation = () => {
+const TopNavigation = (props) => {
+  let likedPhotoExists = false;
+
+  for (const photoId in props.like) {
+    if (props.like[photoId]) {
+      likedPhotoExists = true;
+      break;
+    }
+  }
+
   return (
     <div className="top-nav-bar">
       <span className="top-nav-bar__logo">PhotoLabs</span>
+      <div className="top-nav-bar__topic-groups">
+        <TopicList />
+        <FavBadge isFavPhotoExist={likedPhotoExists} />
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default TopNavigation;
